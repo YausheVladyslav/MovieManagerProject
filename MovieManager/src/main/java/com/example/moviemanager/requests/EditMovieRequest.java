1d6992dfbe22550ffd0aef3ed889beee2a2db990
YaushevVladyslav <i.vlad.yaushev@gmail.com>
2022-12-02 20:22:52 +0200

    edit number validation

--- MovieManager/src/main/java/com/example/moviemanager/requests/EditMovieRequest.java
+++ MovieManager/src/main/java/com/example/moviemanager/requests/EditMovieRequest.java
@@ -1,10 +1,10 @@
 package com.example.moviemanager.requests;
 
 import lombok.Getter;
+import org.hibernate.validator.constraints.Range;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+
 @Getter
 public class EditMovieRequest {
 
@@ -13,7 +13,6 @@ public class EditMovieRequest {
     @NotBlank
     private String genre;
     private int year;
-    @Min(value = 1)
-    @Max(value = 10)
+    @Range(max = 10)
     private int rating;
 }
